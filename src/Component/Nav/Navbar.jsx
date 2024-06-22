a806c65679b8a7744305196cbaded26f1bd26080
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-06-21 20:43:22 -0400

    contact form is on working

--- src/Component/Nav/Navbar.jsx
+++ src/Component/Nav/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
 import "./Navbar.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Navbar() {
   return (
@@ -13,6 +14,7 @@ function Navbar() {
       <li className='nav-list'><NavLink className="nav-link" to="/gallery">Gallery</NavLink></li>
       <li className='nav-list'><NavLink className="nav-link" to="/price">Price</NavLink></li>
       <li className='nav-list'><NavLink className="nav-link" to="/contact">Contact</NavLink></li>
+      <a href="https://www.instagram.com/"><i className="fa-brands fa-instagram"></i></a>
 
       </ul>
 
--- src/Screen/Contact/Contact.jsx
+++ src/Screen/Contact/Contact.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import "./Contact.css"
+import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-container">
+      <div className="contact-div">
+        <div className="contact-detail">
+        <h2 className="contact-name">Hafiz Adnan</h2>
+        <p className="contact-msg">
+          Looking to discuss a project? Please get in touch using the form onthis page. Generally, I’m able to reply to all inquiries within 48hours.
+        </p>
+        <h4 className="contact-email">email@example.com</h4>
+        <h5 className="contact-num">(555) 555-5555</h5>
+        <a className="contact-icon" href="https://www.instagram.com/"><i className="fa-brands fa-instagram"></i></a>
+        <a className="contact-icon" href="https://x.com/"><i className="fa-brands fa-x"></i></a>
+        </div>
+        <div className="contact-form">
+          <form>
+            <label className="form-label">Full Name</label>
+            <input className="form-input" type="text" name="user_name" />
+            <label className="form-label">Email</label>
+            <input className="form-input" typeof="email" name="user_email" />
+            <label className="form-label">Subject: </label>
+            <select>
+              <option value="">Select a subject</option>
+              <option value="Booking Inquiry">Outdoor Shoot</option>
+              <option value="Photo Shoot">Wedding</option>
+              <option value="Collaboration">Event</option>
+              <option value="Other">Other</option>
+            </select>
+            <label className="form-label">Message</label>
+            <textarea name= "message" />
+          </form>
+          <button>Send Message</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
