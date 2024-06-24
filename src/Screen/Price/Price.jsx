8955e6af09fd72a0a9b86c0dfe62e9c3d5fce411
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-06-24 14:44:33 -0400

    contact form and pricing page is almost ready

--- src/Screen/Price/Price.jsx
+++ src/Screen/Price/Price.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+// import nyc from "../../Assets/Backgrounds/nyc.JPG"
+import wedding from "../../Assets/PIctures/DSC09345.JPG"
 import "./Price.css"
 
 function Price() {
   return (
-    <div>Price</div>
+    <div className='price-container'>
+      <div className='title'>
+      <h1>Pricing</h1>
+      <h3>Packages and Products</h3>
+      </div>
+      {/* <img className='price-img' src={nyc} />  */}
+      <div className='price-msg'>
+        <h4 className='price-title'>Time flies, but memories last forever</h4>
+        <p className='price-paragraph'>From the first smile to the adventurous steps into the world, each moment is a treasure. Our photography ensures these precious memories are beautifully preserved, allowing you to relive them for a lifetime.</p>
+      </div>
+      <div className='wed-container'>
+        <img className='price-pic' src={wedding}  />
+        <h2>Wedding Photography <i>-- $00</i></h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, ex?</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, eveniet.</li>
+        </ul>
+      </div>
+      <div className='event-container'>
+      <img className='price-pic' src={wedding}  /> 
+      <h2>Make your Event Captured <i>-- $00</i></h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, ex?</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, eveniet.</li>
+        </ul>
+      </div>
+      <div className='outdoor-container'>
+      <img className='price-pic' src={wedding}  /> 
+      <h2>Outdoor/Indoor Photography <i>-- $00</i></h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, ex?</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, eveniet.</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
